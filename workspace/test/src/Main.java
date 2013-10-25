3a8a2d7bc73b53fe953a06eb6551fe87b71ebde1
zoea2 <zoea19920810@gmail.com>
2013-10-25 11:35:47 +0800

    Update Main.java

--- workspace/test/src/Main.java
+++ workspace/test/src/Main.java
@@ -1,10 +1,10 @@
-
 public class Main {
 	//static double[][] a;
 	public static void main(String[] args){
 		String a = "dsfsd";
 		a += 26;
 		System.out.println("Fuck!!");
+		System.out.println("fuck again!!");
 		System.out.print(a);
 		}
 	public static void MUL(double[][] a,double[][] b){
