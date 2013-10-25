31a866eb8e50fb095f95776cc7b76e4c73389971
zoea2 <zoea19920810@gmail.com>
2013-10-25 11:38:01 +0800

    modified Main.java

--- workspace/test/src/Main.java
+++ workspace/test/src/Main.java
@@ -3,8 +3,6 @@ public class Main {
 	public static void main(String[] args){
 		String a = "dsfsd";
 		a += 26;
-		System.out.println("Fuck!!");
-		sdfsdfsdfsdfsd
 		System.out.print(a);
 		}
 	public static void MUL(double[][] a,double[][] b){
