947bd7b837ba16d81392d118422ec014520771b4
zoea2 <zoea19920810@gmail.com>
2013-10-25 09:09:42 +0800

    update workspace

---
+++ workspace/computational intelligence/src/differentialEvolution/DE.java
@@ -0,0 +1,8 @@
+package differentialEvolution;
+
+
+public class DE {
+	public static void main(String[] args){
+		System.out.println("sjfklsd");
+	}
+}
---
+++ workspace/test/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	//static double[][] a;
+	public static void main(String[] args){
+		String a = "dsfsd";
+		a += 26;
+		System.out.print(a);
+		}
+	public static void MUL(double[][] a,double[][] b){
+	}
+}
