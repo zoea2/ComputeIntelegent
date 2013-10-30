8d615e303af45fd49b8c91078ffc5713b1f3e166
zoea2 <zoea19920810@gmail.com>
2013-10-31 00:48:09 +0800

    update genotype

---
+++ OLDE_CPP/genotype.cpp
@@ -0,0 +1,28 @@
+#include <cstring>
+#include <iostream>
+using namespace std;
+class Genotype{
+public:
+	const static int NVARS = 30;
+	const static double PXOVER = 0.1;
+	const static double SCALE = 0.5;
+	const static int LEVEL = 2;
+	double gene[NVARS];
+	double fitness;
+	double upper[NVARS];
+	double lower[NVARS];
+	Genotype(){
+		memset(gene,0,sizeof(gene));
+		memset(upper,0,sizeof(upper));
+		memset(lower,0,sizeof(lower));
+		fitness = 0;
+	}
+	Genotype(Genotype &genotype){
+		memcpy((&this->gene),(&genotype.gene),sizeof(genotype.gene));	
+		memcpy((&this->upper),(&genotype.upper),sizeof(genotype.upper));
+		memcpy((&this->lower),(&genotype.lower),sizeof(genotype.lower));
+		this->fitness = genotype.fitness;
+	}
+	~Genotype(){
+	}
+};
